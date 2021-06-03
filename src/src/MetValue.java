b9e5c00175e6fafdb50d8169c00fc2937a44eb08
Alexandervsz <alexander_5560@hotmail.com>
2021-06-03 21:43:36 +0200

    Changed metvalue and speed from int to float.

--- src/src/MetValue.java
+++ src/src/MetValue.java
@@ -1,9 +1,9 @@
-public class MetValue {
-    private int metValue;
-    private int speed;
+public class MetValue implements java.io.Serializable {
+    private float metValue;
+    private float speed;
     private String activity;
 
-    public MetValue(int metValue, int speed, String activity) {
+    public MetValue(float metValue, float speed, String activity) {
         this.metValue = metValue;
         this.speed = speed;
         this.activity = activity;
