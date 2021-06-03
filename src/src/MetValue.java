a69907281077f4483ef454923d4f428e9eac1cc7
Alexandervsz <alexander_5560@hotmail.com>
2021-06-03 23:41:29 +0200

    Added speedB renamed speed to speedA added constructor.

--- src/src/MetValue.java
+++ src/src/MetValue.java
@@ -1,12 +1,37 @@
-public class MetValue implements java.io.Serializable {
+public class MetValue {
     private float metValue;
-    private float speed;
+    private float speedA;
+    private float speedB;
     private String activity;
 
-    public MetValue(float metValue, float speed, String activity) {
+    public MetValue(float metValue, float speedA, float speedB, String activity) {
         this.metValue = metValue;
-        this.speed = speed;
+        this.speedA = speedA;
+        this.speedB = speedB;
         this.activity = activity;
+    }
+
+    public MetValue(String metValue, String speedA, String speedB, String activity){
+        this.metValue = Float.parseFloat(metValue);
+        this.speedA = Float.parseFloat(speedA);
+        this.speedB = Float.parseFloat(speedB);
+        this.activity = activity;
+    }
+
+    public float getMetValue() {
+        return metValue;
+    }
+
+    public float getSpeedA() {
+        return speedA;
+    }
+
+    public float getSpeedB() {
+        return speedB;
+    }
+
+    public String getActivity() {
+        return activity;
     }
 
     @Override
