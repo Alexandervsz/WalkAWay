3a94f98c39001af3319c400eb5604ac23baa0929
Alexandervsz <alexander_5560@hotmail.com>
2021-06-03 23:41:58 +0200

    Added constructor added getKilometers

--- src/src/User.java
+++ src/src/User.java
@@ -3,4 +3,16 @@ public class User {
     private float weight;
     private float walkingSpeed;
     private float kcal;
+
+    public User(float mets, float weight, float walkingSpeed, float kcal) {
+        this.mets = mets;
+        this.weight = weight;
+        this.walkingSpeed = walkingSpeed;
+        this.kcal = kcal;
+    }
+
+    public float getKilometers(){
+        float time = kcal * 200 / (mets * 3.5f * weight);
+        return time/60 * walkingSpeed;
+    }
 }
