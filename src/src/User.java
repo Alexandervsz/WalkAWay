0e5832b78e67c75b143754b6f42467a17dd5d7ce
Alexandervsz <alexander_5560@hotmail.com>
2021-06-04 12:54:49 +0200

    Removed unnecessary variables.

--- src/src/User.java
+++ src/src/User.java
@@ -1,15 +1,7 @@
 public class User {
-    private float mets;
-    private float weight;
-    private float walkingSpeed;
-    private float kcal;
     private float distance;
 
     public User(float mets, float weight, float walkingSpeed, float kcal) {
-        this.mets = mets;
-        this.weight = weight;
-        this.walkingSpeed = walkingSpeed;
-        this.kcal = kcal;
         float time = kcal * 200 / (mets * 3.5f * weight);
         this.distance = time/60 * walkingSpeed;
     }
