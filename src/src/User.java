c459d66b19d0fb2e11bae8dbde1e8d5a75ec9b0e
Alexandervsz <alexander_5560@hotmail.com>
2021-06-04 12:06:02 +0200

    Removed getKilometers function, added functionality to constructor.

--- src/src/User.java
+++ src/src/User.java
@@ -3,16 +3,18 @@ public class User {
     private float weight;
     private float walkingSpeed;
     private float kcal;
+    private float distance;
 
     public User(float mets, float weight, float walkingSpeed, float kcal) {
         this.mets = mets;
         this.weight = weight;
         this.walkingSpeed = walkingSpeed;
         this.kcal = kcal;
+        float time = kcal * 200 / (mets * 3.5f * weight);
+        this.distance = time/60 * walkingSpeed;
     }
 
-    public float getKilometers(){
-        float time = kcal * 200 / (mets * 3.5f * weight);
-        return time/60 * walkingSpeed;
+    public float getDistance() {
+        return distance;
     }
 }
