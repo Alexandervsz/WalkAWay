ee46b091eab04d0cc19d3dc07833349c757a296c
Alexandervsz <alexander_5560@hotmail.com>
2021-06-08 22:14:50 +0200

    - Removed unused constructor - Added getters for lat/long - Added equals - Made lat/long final

--- src/src/OsmNode.java
+++ src/src/OsmNode.java
@@ -1,15 +1,34 @@
+import java.util.Objects;
+
 public class OsmNode {
     private final String id;
-    private double lon;
-    private double lat;
-
-    public OsmNode(String id) {
-        this.id = id;
-    }
+    private final double lon;
+    private final double lat;
 
     public OsmNode(String id, double lon, double lat) {
         this.id = id;
         this.lon = lon;
         this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OsmNode node = (OsmNode) o;
+        return Objects.equals(id, node.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
