0722a530456cbd11d00452372b31cb3185c61c3a
Alexandervsz <alexander_5560@hotmail.com>
2021-06-12 16:31:09 +0200

    - Added equals methods - changed type to String[]

--- src/src/Way.java
+++ src/src/Way.java
@@ -1,14 +1,30 @@
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
 
 public class Way {
     public String id;
     public List<Node> nodes;
-    public Map<String, String> type;
+    public String[] type;
 
-    public Way(String id, List<Node> nodes, Map<String, String> type) {
+    public Way(String id, List<Node> nodes, String[] type) {
         this.id = id;
         this.nodes = nodes;
         this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Way way = (Way) o;
+        return Objects.equals(id, way.id) && Objects.equals(nodes, way.nodes) && Arrays.equals(type, way.type);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(id, nodes);
+        result = 31 * result + Arrays.hashCode(type);
+        return result;
     }
 }
