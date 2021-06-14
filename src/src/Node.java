11c7fa66879364d1d71c0ca7becc6c7dcb118970
Alexandervsz <alexander_5560@hotmail.com>
2021-06-14 21:57:42 +0200

    - added bearing and pathnumber.

--- src/src/Node.java
+++ src/src/Node.java
@@ -6,6 +6,8 @@ public class Node implements Comparable<Node> {
     private double lat;
     private double distanceToCurrentNode;
     private Way way;
+    private double bearingToCurrentNode;
+    private int pathnumber;
 
     public String getId() {
         return id;
@@ -19,6 +21,7 @@ public class Node implements Comparable<Node> {
         this.id = id;
         this.lon = lon;
         this.lat = lat;
+        this.bearingToCurrentNode = 0;
     }
 
     public Way getWay() {
@@ -29,20 +32,47 @@ public class Node implements Comparable<Node> {
         this.way = way;
     }
 
-    public void getDistanceTo(Node osmNode) {
+    public void setPathnumber(int pathnumber) {
+        this.pathnumber = pathnumber;
+    }
+
+    public int getPathnumber() {
+        return pathnumber;
+    }
+
+    public void getDistanceTo(Node node) {
         double R = 6371e3;
-        double phi1 = osmNode.getLat() * Math.PI / 180;
+        double phi1 = node.getLat() * Math.PI / 180;
         double phi2 = this.lat * Math.PI / 180;
-        double deltaphi = (this.lat - osmNode.getLat()) * Math.PI / 180;
-        double deltalambda = (this.lon - osmNode.getLon()) * Math.PI / 180;
+        double deltaphi = (this.lat - node.getLat()) * Math.PI / 180;
+        double deltalambda = (this.lon - node.getLon()) * Math.PI / 180;
         double a = Math.sin(deltaphi / 2) * Math.sin(deltaphi / 2) + Math.cos(phi1) * Math.cos(phi2) *
                 Math.sin(deltalambda / 2) * Math.sin(deltalambda / 2);
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         distanceToCurrentNode = R * c;
     }
 
+
+    public void getBearingTo(Node node) {
+        double y = Math.sin(node.getLon() - lon) * Math.cos(node.getLat());
+        double x = Math.cos(lat) * Math.sin(node.getLat()) -
+                Math.sin(lat) * Math.cos(node.getLat()) * Math.cos(node.getLon() - lon);
+        double theta = Math.toDegrees(Math.atan2(y, x));
+        /*if (theta < 0) {
+            theta += 360.0;
+        }*/
+        //bearingToCurrentNode = theta;
+        //theta = (theta * 180 / Math.PI + 360) % 360;
+        bearingToCurrentNode = (theta * 180 / Math.PI + 360) % 360;
+
+    }
+
     public double getDistanceToCurrentNode() {
         return distanceToCurrentNode;
+    }
+
+    public double getBearingToCurrentNode() {
+        return bearingToCurrentNode;
     }
 
     public double getLon() {
@@ -73,7 +103,6 @@ public class Node implements Comparable<Node> {
                 "id='" + id + '\'' +
                 ", lon=" + lon +
                 ", lat=" + lat +
-                ", way=" + way +
                 '}';
     }
 
