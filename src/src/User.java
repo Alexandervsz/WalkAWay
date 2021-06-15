8e8d0901d4f8f3d8a083c68f49254d2f7c368f3e
Alexandervsz <alexander_5560@hotmail.com>
2021-06-15 14:53:32 +0200

    - Added lon and lat as parameters. - Changed constructor to store kcalPerMinute, walkingspeed, lon and lat - Added getEstimetedKcal method.

--- src/src/User.java
+++ src/src/User.java
@@ -1,12 +1,34 @@
 public class User {
-    private final float distance;
+    private final double distance;
+    private final double lon;
+    private final double lat;
+    private final double kcalPerMinute;
+    private final double walkingSpeed;
 
-    public User(float mets, float weight, float walkingSpeed, float kcal) {
-        float time = kcal * 200 / (mets * 3.5f * weight);
-        this.distance = time / 60 * walkingSpeed;
+    public User(double mets, double weight, double walkingSpeed, double kcal, double lon, double lat) {
+        double time = kcal * 200 / (mets * 3.5f * weight);
+        this.kcalPerMinute = mets * 3.5 * weight / 200;
+        this.walkingSpeed = walkingSpeed;
+        this.distance = (time / 60 * walkingSpeed) * 1000;
+        this.lon = lon;
+        this.lat = lat;
     }
 
-    public float getDistance() {
+    public double getDistance() {
         return distance;
     }
+
+    public double getEstimatedKcal(Double distance){
+        return distance / 1000 / walkingSpeed * 60 * kcalPerMinute;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
 }
+
+
