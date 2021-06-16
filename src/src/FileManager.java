da46a3b223a829a297b28a7545686eb20c2850ef
Alexandervsz <alexander_5560@hotmail.com>
2021-06-16 22:14:37 +0200

    - Added javadocs.

--- src/src/FileManager.java
+++ src/src/FileManager.java
@@ -18,6 +18,16 @@ import java.util.Date;
 import java.util.List;
 
 public class FileManager {
+
+    /**
+     * Fetches all nodes and ways in a bbox, which is based on the begin node and total distance required to walk.
+     * @param currentNode The node for which the ways must be fetched.
+     * @param totalDistance The total required distance of the path.
+     * @return A JSONObject with all the paths around the begin node.
+     * @throws IOException If the html request is invalid somehow.
+     * @throws InterruptedException If the html request is interrupted.
+     * @throws ParseException If the JSON is incorrect.
+     */
     public JSONObject getOverpassData(Node currentNode, double totalDistance) throws IOException, InterruptedException, ParseException {
         String bbox = generateBbox(currentNode, totalDistance);
         DatabaseManager databaseManager = new DatabaseManager();
@@ -51,9 +61,15 @@ public class FileManager {
         JSONParser parser = new JSONParser();
         Object obj = parser.parse(response.body());
         return (JSONObject) obj;
-
     }
 
+    /**
+     * Generates a bbox, which is required in an overpass request. It generates a bbox of totaldistance * totaldistance
+     * so even in the worst case scenario (straight way from A to B) enough data is available.
+     * @param currentNode The center node to draw the bbox around.
+     * @param totalDistance The total required distance of the path.
+     * @return A string to be used in further processing.
+     */
     public String generateBbox(Node currentNode, double totalDistance) {
         double lat = currentNode.getLat();
         double lon = currentNode.getLon();
@@ -65,11 +81,16 @@ public class FileManager {
         double lat1 = lat + dLat * 180 / Math.PI;
         double lon1 = lon + dLon * 180 / Math.PI;
         return latO + "," + lonO + "," + lat1 + "," + lon1;
-
     }
 
+    /**
+     * Generates a gpx files based on the parameters.
+     * @param file The file object to write to.
+     * @param name The name of the path in the gpx file.
+     * @param points The path to base the gpx file on, ordered from start to finish.
+     */
     public void generateGpx(File file, String name, List<Node> points) {
-        String header = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\" ?><gpx xmlns=\"http://www.topografix.com/GPX/1/1\" creator=\"MapSource 6.15.5\" version=\"1.1\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"  xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd\"><trk>\n";
+        String header = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\" ?><gpx xmlns=\"https://www.topografix.com/GPX/1/1\" creator=\"MapSource 6.15.5\" version=\"1.1\" xmlns:xsi=\"https://www.w3.org/2001/XMLSchema-instance\"  xsi:schemaLocation=\"https://www.topografix.com/GPX/1/1 https://www.topografix.com/GPX/1/1/gpx.xsd\"><trk>\n";
         name = "<name>" + name + "</name><trkseg>\n";
         StringBuilder segments = new StringBuilder();
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
@@ -89,5 +110,4 @@ public class FileManager {
             System.out.println("Error Writting Path" + e);
         }
     }
-
 }
