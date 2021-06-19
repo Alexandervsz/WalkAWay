67f6696fe83ebe38d2fc1df33543aa0c88f6aac9
Alexandervsz <alexander_5560@hotmail.com>
2021-06-19 22:10:53 +0200

    - Rewrote pathfinding algorithm. - Added PathNotFound screen. - Removed nodeset since it's no longer used. - Renamed distanceTraveled to totalDistance, and totaldistance to requiredDistance. - Removed unused variables.

--- src/src/PathFinder.java
+++ src/src/PathFinder.java
@@ -6,31 +6,30 @@ import java.util.*;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.json.simple.parser.ParseException;
 
 import javax.swing.*;
 
 public class PathFinder {
-    private Set<Node> nodeSet;
     private Set<Way> waySet;
     private final User user;
-    private double distanceTraveled;
-    private Node previousNode;
-    private Node beginNode;
+    private Way previousWay;
+    private final Node beginNode;
     private Node currentNode;
-    private Way beginWay;
-    private final double totalDistance;
+    private final double requiredDistance;
+    private double totalDistance;
     private final List<Node> path;
 
     public PathFinder(User user) {
         this.user = user;
-        this.nodeSet = new HashSet<>();
         this.path = new ArrayList<>();
-        this.beginWay = null;
-        this.distanceTraveled = 0;
+        this.totalDistance = 0;
         this.beginNode = new Node("start", user.getLon(), user.getLat());
-        this.previousNode = beginNode;
+        Way beginWay = new Way("start");
+        this.currentNode = beginNode;
+        this.previousWay = beginWay;
         path.add(beginNode);
-        this.totalDistance = user.getDistance();
+        this.requiredDistance = user.getDistance();
     }
 
     public void start() {
@@ -39,9 +38,8 @@ public class PathFinder {
         SwingWorker<Void, Void> worker = new SwingWorker<>() {
 
             @Override
-            protected Void doInBackground() throws Exception {
-                fileManager.getOverpassData(beginNode, totalDistance, dialog);
-                nodeSet = fileManager.getNodeSet();
+            protected Void doInBackground() throws IOException, ParseException, InterruptedException {
+                fileManager.getOverpassData(beginNode, requiredDistance, dialog);
                 waySet = fileManager.getWaySet();
                 dialog.setProgress(60);
                 dialog.setText(" Generating path...");
@@ -61,68 +59,98 @@ public class PathFinder {
     }
 
     public void getRoute() {
-        currentNode = previousNode;
-        while (distanceTraveled < totalDistance) {
-            for (Node node : nodeSet) {
-                node.getDistanceTo(previousNode);
-                node.getBearingTo(previousNode);
+        while (totalDistance < requiredDistance) {
+            Way currentWay = getClosestWay(currentNode);
+            currentWay.setPreviousWay(previousWay);
+            while (currentWay.equals(new Way("empty"))) {
+                if (currentWay.getDirection() == 1) {
+                    NavigableMap<Integer, Node> waynodes = currentWay.getNodePositionsReversed();
+                    revertPath(waynodes, currentWay);
+                } else if (currentWay.getDirection() == -1) {
+                    TreeMap<Integer, Node> waynodes = currentWay.getNodePositions();
+                    revertPath(waynodes, currentWay);
+                }
+                currentWay = currentWay.getPreviousWay();
+                if (currentWay.equals(new Way("start"))) {
+                    new PathNotFound();
+                    System.exit(0);
+                }
+                currentWay = getClosestWay(currentWay.getLastNode());
             }
-            Way closestWay = getClosestWay(currentNode);
-            if (closestWay == null){
-                System.out.println("path not found");
-                return;
-            }
-            currentNode = closestWay.getClosestNode(currentNode);
-            walkToNextNode(currentNode);
-            processWay(closestWay);
+            waySet.remove(currentWay);
+            Node closestNode = currentWay.getClosestNode(currentNode);
+            closestNode.getDistanceTo(currentNode);
+            totalDistance += closestNode.getDistanceToCurrentNode();
+            path.add(closestNode);
+            processWay(closestNode, currentWay);
         }
     }
 
-    public Way getClosestWay(Node target){
+    public void revertPath(Map<Integer, Node> map, Way currentWay) {
+        for (Map.Entry<Integer, Node> entry : map.entrySet()) {
+            if (entry.getKey() == currentWay.getEntryPoint()) {
+                break;
+            }
+            entry.getValue().getDistanceTo(currentNode);
+            path.remove(entry.getValue());
+            totalDistance = totalDistance - entry.getValue().getDistanceToCurrentNode();
+            currentNode = entry.getValue();
+        }
+    }
+
+    public Way getClosestWay(Node target) {
         double closest = Double.MAX_VALUE;
         Way closestWay = null;
-        for (Way way: waySet){
+        for (Way way : waySet) {
             Node closestWayNode = way.getClosestNode(target);
-            if (closestWayNode.getDistanceToCurrentNode() < closest){
+            if (closestWayNode.getDistanceToCurrentNode() < closest) {
                 closest = closestWayNode.getDistanceToCurrentNode();
                 closestWay = way;
             }
-
         }
-        return closestWay;
+        if (closest > 60) {
+            return new Way("empty");
+        } else {
+            return closestWay;
+        }
     }
 
-    public void processWay(Way currentWay) {
-        TreeMap<Integer, Node> nodesInWay = currentWay.getNodePositions();
-        int midpoint = (nodesInWay.lastKey() - nodesInWay.firstKey()) / 2;
-            for (int x = currentWay.getPositionOfNode(currentNode); x < nodesInWay.lastKey(); x++){
-                Node node = nodesInWay.get(x);
-                if (distanceTraveled >= totalDistance) {
-                        return;
-                    }
-                walkToNextNode(node);
+    public void processWay(Node entryNode, Way currentWay) {
+        TreeMap<Integer, Node> nodeMap = currentWay.getNodePositions();
+        int midpoint = nodeMap.firstKey() + nodeMap.lastKey() / 2;
+        int entry = currentWay.getPositionOfNode(entryNode);
+        currentWay.setEntryPoint(entry);
+        if (entry > midpoint) {
+            currentWay.setDirection(-1);
+        } else {
+            currentWay.setDirection(1);
+        }
+        if (currentWay.getDirection() == 1) {
+            previousWay = currentWay;
+            walkPath(currentWay.getNodePositionsFromPoint(entry), entryNode);
+        } else {
+            previousWay = currentWay;
+            walkPath(currentWay.getNodePositionsFromPointReversed(entry), entryNode);
+        }
 
-            }
-
-
-
-        waySet.remove(currentWay);
     }
 
-    public void walkToNextNode(Node newNode) {
-        nodeSet.remove(newNode);
-        newNode.getDistanceTo(currentNode);
-        distanceTraveled += newNode.getDistanceToCurrentNode();
-        path.add(newNode);
-        currentNode = newNode; // If called by path.
-        previousNode = newNode; // If called by begin of loop.
+    public void walkPath(SortedMap<Integer, Node> wayPath, Node entryNode) {
+        Node currentPathNode = entryNode;
+        wayPath.remove(wayPath.firstKey());
+        for (Map.Entry<Integer, Node> entry : wayPath.entrySet()) {
+            totalDistance += entry.getValue().getDistanceTo(currentPathNode);
+            path.add(entry.getValue());
+            currentPathNode = entry.getValue();
+        }
+        currentNode = currentPathNode;
 
     }
 
     public void showOutput() throws IOException {
         DecimalFormat decimalFormat = new DecimalFormat("#.##");
-        String distance = String.valueOf(decimalFormat.format(distanceTraveled));
-        String calories = String.valueOf(decimalFormat.format(user.getEstimatedKcal(distanceTraveled)));
+        String distance = String.valueOf(decimalFormat.format(totalDistance));
+        String calories = String.valueOf(decimalFormat.format(user.getEstimatedKcal(totalDistance)));
         new OutputScreen(distance, calories);
         File file = new File("path/walking_route.gpx");
         FileManager fileManager = new FileManager();
@@ -147,7 +175,7 @@ public class PathFinder {
     }
 
     public static void main(String[] args) {
-        User user = new User(6.0, 70.0, 6.4, 40, 5.072073, 52.645407);
+        User user = new User(6.0, 70.0, 6.4, 50, 5.072073, 52.645407);
         PathFinder pathFinder = new PathFinder(user);
         pathFinder.start();
     }
