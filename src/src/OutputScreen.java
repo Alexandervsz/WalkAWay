bc99ec7cca66b12c08ed65e4aed5d5b0b0ca09df
Alexandervsz <alexander_5560@hotmail.com>
2021-06-20 13:27:02 +0200

    - Implemented time label.

--- src/src/OutputScreen.java
+++ src/src/OutputScreen.java
@@ -7,12 +7,14 @@ public class OutputScreen extends JFrame{
     private JLabel caloriesLabel;
     private JButton closeButton;
     private JPanel outputPanel;
+    private JLabel timeLabel;
 
-    public OutputScreen(String distance, String calories){
+    public OutputScreen(String distance, String calories, String time){
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         distanceLabel.setText("Total distance of path: "+distance);
         caloriesLabel.setText("Estimated amount of calories burnt when walking this route: "+calories);
+        timeLabel.setText("Estimated duration of activity: "+ time+" minutes.");
         closeButton.setText("Close");
         closeButton.addActionListener(this::stop);
         setLayout(new GridLayout());
