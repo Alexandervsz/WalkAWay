914be6942678960ddca83af84248a1597b6e455c
Alexandervsz <alexander_5560@hotmail.com>
2021-06-20 14:35:45 +0200

    - Added Javadocs.

--- src/src/PathFinder.java
+++ src/src/PathFinder.java
@@ -32,6 +32,9 @@ public class PathFinder {
         this.requiredDistance = user.getDistance();
     }
 
+    /**
+     * This starts the pathfinding algorithm. Also updates the progress bar, therefore a second thread is required.
+     */
     public void start() {
         FileManager fileManager = new FileManager();
         final LoadingDialog dialog = new LoadingDialog(" Loading overpass data...");
@@ -58,6 +61,13 @@ public class PathFinder {
         dialog.setVisible(true);
     }
 
+    /**
+     * This program files a route by first getting the closest node and then finding out which way has the closest node.
+     * Then, it "walks" to the next node (adding the distance it walked to the total) and follows the path from
+     * the node it entered. Once the path has been traversed, it checks whether the required distance has been met,
+     * and starts over again if needed. Also, when it meets a dead end, it follows the routes back to either start
+     * (no path found) or until a viable path is found.
+     */
     public void getRoute() {
         while (totalDistance < requiredDistance) {
             Way currentWay = getClosestWay(currentNode);
@@ -86,6 +96,12 @@ public class PathFinder {
         }
     }
 
+    /**
+     * This function removes the distance traveled along a path up until the point it was entered.
+     *
+     * @param map        The nodes of the way.
+     * @param currentWay The current way being processed.
+     */
     public void revertPath(Map<Integer, Node> map, Way currentWay) {
         for (Map.Entry<Integer, Node> entry : map.entrySet()) {
             if (entry.getKey() == currentWay.getEntryPoint()) {
@@ -98,6 +114,12 @@ public class PathFinder {
         }
     }
 
+    /**
+     * Returns the closest way of all the ways in the current set.
+     *
+     * @param target The target node of which the distance is required.
+     * @return The way with the closest node in it.
+     */
     public Way getClosestWay(Node target) {
         double closest = Double.MAX_VALUE;
         Way closestWay = null;
@@ -116,6 +138,13 @@ public class PathFinder {
         }
     }
 
+    /**
+     * Cuts the treemap down to the right position and length, based on the direction the algorithm is going,
+     * and where the algorithm entered the path.
+     *
+     * @param entryNode  The node where the algorithm entered the path.
+     * @param currentWay The current way being processed.
+     */
     public void processWay(Node entryNode, Way currentWay) {
         TreeMap<Integer, Node> nodeMap = currentWay.getNodePositions();
         int midpoint = nodeMap.firstKey() + nodeMap.lastKey() / 2;
@@ -136,10 +165,19 @@ public class PathFinder {
 
     }
 
+    /**
+     * Processes the required nodes. Adds their distance to the total, and the nodes themselves to the path.
+     *
+     * @param wayPath   The processed treemap.
+     * @param entryNode The node where the algorithm entered the path.
+     */
     public void walkPath(SortedMap<Integer, Node> wayPath, Node entryNode) {
         Node currentPathNode = entryNode;
         wayPath.remove(wayPath.firstKey());
         for (Map.Entry<Integer, Node> entry : wayPath.entrySet()) {
+            if (totalDistance >= requiredDistance) {
+                return;
+            }
             totalDistance += entry.getValue().getDistanceTo(currentPathNode);
             path.add(entry.getValue());
             currentPathNode = entry.getValue();
@@ -148,6 +186,11 @@ public class PathFinder {
 
     }
 
+    /**
+     * Generates a GPX file, shows a screen with information about the generated path, and the generated path itself.
+     *
+     * @throws IOException When the html template cannot be interpreted correctly.
+     */
     public void showOutput() throws IOException {
         DecimalFormat decimalFormat = new DecimalFormat("#.##");
         DecimalFormat timeFormat = new DecimalFormat("#.#");
@@ -178,7 +221,7 @@ public class PathFinder {
     }
 
     public static void main(String[] args) {
-        User user = new User(6.0, 70.0, 6.4, 200, 5.061843, 52.650095, true);
+        User user = new User(6.0, 70.0, 6.4, 200, 5.061843, 52.650095, false);
         PathFinder pathFinder = new PathFinder(user);
         pathFinder.start();
     }
