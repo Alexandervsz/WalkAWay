08df84c447d1677f97179dd0d212ec349017c11b
Alexandervsz <alexander_5560@hotmail.com>
2021-06-20 14:37:04 +0200

    - Removed type attribute. - Added docstrings.

--- src/src/Way.java
+++ src/src/Way.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class Way {
     private final String id;
-    //private final String[] type;
     private final TreeMap<Integer, Node> nodePositions;
     private final Set<Node> nodeSet;
     private Way previousWay;
@@ -11,16 +10,27 @@ public class Way {
 
     public Way(String id) {
         this.id = id;
-        //this.type = type;
         nodePositions = new TreeMap<>();
         nodeSet = new HashSet<>();
     }
 
+    /**
+     * Called when the json file is being processed, and is setting up the way.
+     *
+     * @param position The position of the node in the way.
+     * @param node     Part of the way.
+     */
     public void addNode(int position, Node node) {
         nodePositions.put(position, node);
         nodeSet.add(node);
     }
 
+    /**
+     * Returns the closest node in the way, in relation to target node.
+     *
+     * @param target The node to be compared against.
+     * @return The node with the smallest distance.
+     */
     public Node getClosestNode(Node target) {
         for (Node node : nodeSet) {
             node.getDistanceTo(target);
@@ -28,25 +38,62 @@ public class Way {
         return Collections.min(nodeSet);
     }
 
+    /**
+     * The pathfinding algorithm uses this to decide if it's traveling the way forwards or backwards.
+     *
+     * @return The direction the algorithm is currently heading (1 for forward, -1 for backward.)
+     */
     public int getDirection() {
         return direction;
     }
 
+    /**
+     * Gets the last node in the path,
+     *
+     * @return The last node relative to the direction the path was traveled.
+     */
     public Node getLastNode() {
-        return nodePositions.lastEntry().getValue();
+        if (direction == 1) {
+            return nodePositions.lastEntry().getValue();
+        } else {
+            return nodePositions.firstEntry().getValue();
+        }
     }
 
-    public Node getFirstNode() {
-        return nodePositions.firstEntry().getValue();
-    }
-
-    public int getPositionOfNode(Node node) {
+    /**
+     * Returns the position of the node in the way.
+     *
+     * @param target The target node.
+     * @return The position of the target.
+     */
+    public int getPositionOfNode(Node target) {
         for (Map.Entry<Integer, Node> entry : nodePositions.entrySet()) {
-            if (entry.getValue().equals(node)) {
+            if (entry.getValue().equals(target)) {
                 return entry.getKey();
             }
         }
         return -1;
+    }
+
+    /**
+     * Returns the nodes of the way starting from the given point.
+     *
+     * @param point The start point
+     * @return A map with the required nodes and their positions.
+     */
+    public SortedMap<Integer, Node> getNodePositionsFromPoint(int point) {
+        return nodePositions.tailMap(point);
+    }
+
+    /**
+     * Returns the nodes of the way starting from the given point, if entered from the last point.
+     *
+     * @param point The start point
+     * @return A map with the required nodes and their positions.
+     */
+    public SortedMap<Integer, Node> getNodePositionsFromPointReversed(int point) {
+        NavigableMap<Integer, Node> reverse = nodePositions.descendingMap();
+        return reverse.tailMap(point);
     }
 
     public TreeMap<Integer, Node> getNodePositions() {
@@ -69,24 +116,11 @@ public class Way {
         return Objects.equals(id, way.id);
     }
 
-    /*public String[] getType() {
-        return type;
-    }*/
-
     @Override
     public int hashCode() {
         int result = Objects.hash(id);
         result = 31 * result;
         return result;
-    }
-
-    public SortedMap<Integer, Node> getNodePositionsFromPoint(int point) {
-        return nodePositions.tailMap(point);
-    }
-
-    public SortedMap<Integer, Node> getNodePositionsFromPointReversed(int point) {
-        NavigableMap<Integer, Node> reverse = nodePositions.descendingMap();
-        return reverse.tailMap(point);
     }
 
     public Way getPreviousWay() {
