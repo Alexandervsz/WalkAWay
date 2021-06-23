7f6cd81b5c69122aef8217520adf0d65dfb2c9c5
Alexandervsz <alexander_5560@hotmail.com>
2021-06-23 14:16:33 +0200

    - Made variables final.

--- test/NodeTests.java
+++ test/NodeTests.java
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.Assertions;
  * Test class for algorithm.Node class.
  */
 public class NodeTests {
-    Node test1 = new Node("test", 5.0650352, 52.650352);
-    Node test2 = new Node("test", 5.0650627, 52.6502963);
-    Node test3 = new Node("test", 5.0650627, 53.6502963);
-    Node test4 = new Node("test", 5.0657671, 52.6504847);
-    Node test5 = new Node("test", 5.0659635, 52.6505343);
-    Node test6 = new Node("test2", 5.0650352, 52.650352);
+    final Node test1 = new Node("test", 5.0650352, 52.650352);
+    final Node test2 = new Node("test", 5.0650627, 52.6502963);
+    final Node test3 = new Node("test", 5.0650627, 53.6502963);
+    final Node test4 = new Node("test", 5.0657671, 52.6504847);
+    final Node test5 = new Node("test", 5.0659635, 52.6505343);
+    final Node test6 = new Node("test2", 5.0650352, 52.650352);
 
     /**
      * Tests whether the getDistanceTo function is has an error rate of at most 3%.
